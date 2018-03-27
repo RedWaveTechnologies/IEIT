ccfab18a392c371231b96be5909ae4b51ba2455c
cyberlogic <redwavetechnologiesgroup@gmail.com>
2018-03-27 13:52:13 +0200

    First commit

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -12,7 +12,7 @@ namespace parameters {
 
 const uint64_t CRYPTONOTE_MAX_BLOCK_NUMBER                   = 500000000;
 const size_t   CRYPTONOTE_MAX_BLOCK_BLOB_SIZE                = 500000000;
-const size_t   CRYPTONOTE_MAX_TX_SIZE                        = 1000000000;
+const size_t   CRYPTONOTE_MAX_TX_SIZE                        = 2000000000;
 //TODO Currency-specific address prefix
 const uint64_t CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX       = 0xfb;
 //TODO Choose maturity period for your currency
@@ -24,23 +24,24 @@ const size_t   BLOCKCHAIN_TIMESTAMP_CHECK_WINDOW             = 60;
 //TODO Specify total number of available coins
 //TODO ((uint64_t)(-1)) equals to 18446744073709551616 coins
 //TODO or you can define number explicitly UINT64_C(858986905600000000)
-const uint64_t MONEY_SUPPLY                                  = UINT64_C(792100000000000000);
-const uint64_t GENESIS_BLOCK_REWARD                          = UINT64_C(200000000000000000);
+const uint64_t MONEY_SUPPLY                                  = UINT64_C(10000000000);
+const uint64_t GENESIS_BLOCK_REWARD                          = UINT64_C(2000000000);
+const size_t   SYNC_FROM_ZERO                                = 1;
 const unsigned EMISSION_SPEED_FACTOR                         = 19;
 static_assert(EMISSION_SPEED_FACTOR <= 8 * sizeof(uint64_t), "Bad EMISSION_SPEED_FACTOR");
 
 //TODO Define number of blocks for block size median calculation
 const size_t   CRYPTONOTE_REWARD_BLOCKS_WINDOW               = 100;
-const size_t   CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE     = 20000; //size of block (bytes) after which reward for block calculated using block size
+const size_t   CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE     = 10000; //size of block (bytes) after which reward for block calculated using block size
 const size_t   CRYPTONOTE_COINBASE_BLOB_RESERVED_SIZE        = 600;
 //TODO Define number of digits
-const size_t   CRYPTONOTE_DISPLAY_DECIMAL_POINT              = 8;
+const size_t   CRYPTONOTE_DISPLAY_DECIMAL_POINT              = 0;
 //TODO Define minimum fee for transactions
-const uint64_t MINIMUM_FEE                                   = 100000;
+const uint64_t MINIMUM_FEE                                   = 1;
 const uint64_t DEFAULT_DUST_THRESHOLD                        = MINIMUM_FEE;
 
 //TODO Define preferred block's target time
-const uint64_t DIFFICULTY_TARGET                             = 240; // seconds
+const uint64_t DIFFICULTY_TARGET                             = 120; // seconds
 const uint64_t EXPECTED_NUMBER_OF_BLOCKS_PER_DAY             = 24 * 60 * 60 / DIFFICULTY_TARGET;
 //TODO There are options to tune CryptoNote's difficulty retargeting function.
 //TODO We recommend not to change it.
@@ -75,7 +76,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "ieit_conf.json";
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "IEIT";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff0001d2b5e09afc2b029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121014960695da2d5ebb2a55475cc2252d3e363e387a954ae06cdf5191ce6d29fe434";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff0001819501029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101d57a8373a185f88b3ed24dac3d952411ca4cd1548bc04a3bb52c33114eb38641";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
